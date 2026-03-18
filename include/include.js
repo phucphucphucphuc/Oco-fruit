bc8856da2eea404d2de4f171d2ff2b97bbfbff6c
phucphucphucphuc <dangphucminh13@gmail.com>
2026-03-18 07:51:07 +0700

    font-update

--- include/include.js
+++ include/include.js
@@ -1,21 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   // Load nav
-//   fetch("ui/nav.html")
-//     .then(response => response.text())
-//     .then(html => {
-//       document.getElementById("nav-placeholder").innerHTML = html;
-//     })
-//     .catch(err => console.error("Cannot load nav", err));
-
-//   // Load footer
-//   fetch("ui/footer.html")
-//     .then(response => response.text())
-//     .then(html => {
-//       document.getElementById("footer-placeholder").innerHTML = html;
-//     })
-//     .catch(err => console.error("Cannot load footer", err));
-// });
-
 document.addEventListener("DOMContentLoaded", () => {
     
     //head
